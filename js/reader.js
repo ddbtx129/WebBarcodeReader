7a60d48f17702e7786948f2d1d85391d8b5bd0c9
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2022-09-18 20:35:37 +0900

    Add files via upload

--- js/reader.js
+++ js/reader.js
@@ -1,18 +1,21 @@
-﻿var btn = document.getElementById('btn');
+﻿var barcode = document.getElementById('barcode');
+var qrrcode = document.getElementById('qrcode');
 var reset = document.getElementById('reset');
+var anser = document.getElementById('anser');
+var button = document.getElementById('button');
 
 reset.addEventListener('click', () => {
 
-    btn.style.display = "inline";
+    barcode.style.display = "inline";
     reset.style.display = "none";
     document.getElementById("code").style.display = "none";
 
     location.reload();
 });
 
-btn.addEventListener('click', () => {
+barcode.addEventListener('click', () => {
 
-    btn.style.display = "none";
+    button.style.display = "none";
 
     var scanarea = document.createElement('div');
     scanarea.id = "scanarea";
@@ -28,11 +31,12 @@ btn.addEventListener('click', () => {
     info.innerHTML = "カメラにバーコードを写してください。";
     scanarea.appendChild(info);
 
-    var DetectedCount = 0, DetectedCode = "";
     var video, tmp, tmp_ctx, value, prev, prev_ctx, w, h, mw, mh, x1, y1;
-    var id;
 
     scanarea.style.display = 'block';
+
+    var DetectedCount = 0, DetectedCode = "";
+    var id;
 
     video = document.createElement('video');
     video.id = "video";
@@ -40,13 +44,20 @@ btn.addEventListener('click', () => {
     video.setAttribute("muted", "");
     video.setAttribute("playsinline", "");
     video.onloadedmetadata = function (e) { video.play(); };
+
     prev = document.getElementById("preview");
     prev_ctx = prev.getContext("2d");
+
     tmp = document.createElement('canvas');
     tmp_ctx = tmp.getContext("2d");
+
     value = document.getElementById("code");
     value.value = "";
+
+    value.style.height = "36px";
     value.style.display = "none";
+    value.style.overflow = "hidden";
+    value.style.textAlign = "center";
 
     //カメラ使用の許可ダイアログが表示される
     navigator.mediaDevices.getUserMedia(
@@ -134,10 +145,11 @@ btn.addEventListener('click', () => {
             DetectedCode = result.codeResult.code;
         }
         if (DetectedCount >= 3) {
-            console.log(result.codeResult.code);
 
-            value.style.display = "inline";
-            reset.style.display = "inline";
+            Quagga.stop();
+            clearTimeout(id);
+
+            //console.log(result.codeResult.code);
 
             value.value = result.codeResult.code;
             displayreset();
@@ -148,11 +160,130 @@ btn.addEventListener('click', () => {
 
         DetectedCode = '';
         DetectedCount = 0;
+
+        button.style.display = "inline";
         scanarea.style.display = 'none';
 
-        clearTimeout(id);
+        tmp.remove();
 
-        Quagga.stop();
+        video.stop();
+        video.srcObject = null;
+
+        video.remove();
+        scanarea.remove();
+    }
+});
+
+
+qrrcode.addEventListener('click', () => {
+
+    button.style.display = "none";
+
+    var scanarea = document.createElement('div');
+    scanarea.id = "scanarea";
+    scanarea.className = "scanarea";
+    document.body.appendChild(scanarea);
+
+    var preview = document.createElement('canvas');
+    preview.id = "preview";
+    scanarea.appendChild(preview);
+
+    var info = document.createElement('p');
+    info.className = "text";
+    info.innerHTML = "カメラにバーコードを写してください。";
+    scanarea.appendChild(info);
+
+    var video, tmp, tmp_ctx, valuie, prev, prev_ctx, w, h, m, x1, y1;
+
+    scanarea.style.display = 'block';
+
+    var id;
+
+    video = document.createElement('video');
+    video.setAttribute("autoplay", "");
+    video.setAttribute("muted", "");
+    video.setAttribute("playsinline", "");
+    video.onloadedmetadata = function (e) { video.play(); };
+
+    prev = document.getElementById("preview");
+    prev_ctx = prev.getContext("2d");
+
+    tmp = document.createElement('canvas');
+    tmp_ctx = tmp.getContext("2d");
+
+    valuie = document.getElementById("code");
+    value.value = "";
+
+    value.style.height = "72px";
+    value.style.display = "none";
+    value.style.textAlign = "left";
+
+    //カメラ使用の許可ダイアログが表示される
+    navigator.mediaDevices.getUserMedia(
+        //マイクはオフ, カメラの設定   できれば背面カメラ    できれば640×480
+        { "audio": false, "video": { "facingMode": "environment", "width": { "ideal": 720 }, "height": { "ideal": 480 } } }
+    ).then( //許可された場合
+        function (stream) {
+            video.srcObject = stream;
+            //0.5秒後にスキャンする
+            id = setTimeout(Scan, 500);
+        }
+    ).catch( //許可されなかった場合
+        function (err) { value.innerHTML = value.innerHTML + err + '\n'; }
+    );
+
+    function Scan() {
+
+        //選択された幅高さ
+        w = video.videoWidth;
+        h = video.videoHeight;
+
+        //画面上の表示サイズ
+        prev.style.width = (w / 2) + "px";
+        prev.style.height = (h / 2) + "px";
+
+        //内部のサイズ
+        prev.setAttribute("width", w);
+        prev.setAttribute("height", h);
+
+        if (w > h) { m = h * 0.5; } else { m = w * 0.5; }
+
+        x1 = (w - m) / 2;
+        y1 = (h - m) / 2;
+
+        prev_ctx.drawImage(video, 0, 0, w, h);
+        prev_ctx.beginPath();
+        prev_ctx.strokeStyle = "rgb(255,0,0)";
+        prev_ctx.lineWidth = 2;
+        prev_ctx.rect(x1, y1, m, m);
+        prev_ctx.stroke();
+
+        tmp.setAttribute("width", m);
+        tmp.setAttribute("height", m);
+        tmp_ctx.drawImage(prev, x1, y1, m, m, 0, 0, m, m);
+
+        let imageData = tmp_ctx.getImageData(0, 0, m, m);
+
+        let scanResult = jsQR(imageData.data, m, m);
+
+        if (scanResult) {
+
+            clearTimeout(id);
+
+            //QRコードをスキャンした結果を出力
+            value.value = scanResult.data;
+            value.scrollTop = value.scrollHeight;
+
+            displayreset();
+        }
+
+    //    setTimeout(Scan, 200);
+    }
+
+    function displayreset() {
+
+        button.style.display = "inline";
+        scanarea.style.display = 'none';
 
         tmp.remove();
 
