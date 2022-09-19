70ff19d5be109b81417dadcb21c08160c1eb0f18
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2022-09-19 14:00:56 +0900

    Add files via upload

--- js/reader.js
+++ js/reader.js
@@ -83,7 +83,7 @@ barcode.addEventListener('click', () => {
 
     function Scan(first) {
 
-        var SizeRate = 0.5;
+        var SizeRate = 0.485;
         var ScanRate = new Array(0.6, 0.25);
 
         if (first) {
@@ -137,6 +137,48 @@ barcode.addEventListener('click', () => {
         });
         id = setTimeout(Scan, 50, false);
     }
+
+    Quagga.onProcessed(function (result) {
+        var drawingCtx = Quagga.canvas.ctx.overlay,
+            drawingCanvas = Quagga.canvas.dom.overlay;
+
+        if (result) {
+            if (result.boxes) {
+                drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+                result.boxes.filter(function (box) {
+                    return box !== result.box;
+                }).forEach(function (box) {
+                    Quagga.ImageDebug.drawPath(box, {
+                        x: 0,
+                        y: 1
+                    }, drawingCtx, {
+                        color: "green",
+                        lineWidth: 2
+                    });
+                });
+            }
+
+            if (result.box) {
+                Quagga.ImageDebug.drawPath(result.box, {
+                    x: 0,
+                    y: 1
+                }, drawingCtx, {
+                    color: "#00F",
+                    lineWidth: 2
+                });
+            }
+
+            if (result.codeResult && result.codeResult.code) {
+                Quagga.ImageDebug.drawPath(result.line, {
+                    x: 'x',
+                    y: 'y'
+                }, drawingCtx, {
+                    color: 'red',
+                    lineWidth: 3
+                });
+            }
+        }
+    });
 
     Quagga.onDetected(function (result) {
         //読み取り誤差が多いため、3回連続で同じ値だった場合に成功とする
