17316eb652b5e0fdab5cf08db0583553c1903417
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2022-09-19 14:10:30 +0900

    Add files via upload

--- js/reader.js
+++ js/reader.js
@@ -139,43 +139,26 @@ barcode.addEventListener('click', () => {
     }
 
     Quagga.onProcessed(function (result) {
-        var drawingCtx = Quagga.canvas.ctx.overlay,
-            drawingCanvas = Quagga.canvas.dom.overlay;
+        const drawingCtx = Quagga.canvas.ctx.overlay;
+        const drawingCanvas = Quagga.canvas.dom.overlay;
 
         if (result) {
+            // 検出中の緑の線の枠
             if (result.boxes) {
                 drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
                 result.boxes.filter(function (box) {
                     return box !== result.box;
                 }).forEach(function (box) {
-                    Quagga.ImageDebug.drawPath(box, {
-                        x: 0,
-                        y: 1
-                    }, drawingCtx, {
-                        color: "green",
-                        lineWidth: 2
-                    });
+                    Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
                 });
             }
-
+            // 読込中の青枠
             if (result.box) {
-                Quagga.ImageDebug.drawPath(result.box, {
-                    x: 0,
-                    y: 1
-                }, drawingCtx, {
-                    color: "#00F",
-                    lineWidth: 2
-                });
+                Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
             }
-
+            // 検出完了時の赤線
             if (result.codeResult && result.codeResult.code) {
-                Quagga.ImageDebug.drawPath(result.line, {
-                    x: 'x',
-                    y: 'y'
-                }, drawingCtx, {
-                    color: 'red',
-                    lineWidth: 3
-                });
+                Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
             }
         }
     });
@@ -190,13 +173,11 @@ barcode.addEventListener('click', () => {
         }
         if (DetectedCount >= 3) {
 
-            //Quagga.stop();
-            //clearTimeout(id);
-
-            //console.log(result.codeResult.code);
+            Quagga.stop();
+            clearTimeout(id);
 
             value.value = result.codeResult.code;
-            //displayreset();
+            displayreset();
         }
     });
 
