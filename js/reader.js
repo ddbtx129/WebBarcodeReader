88a3f46f2d766c93dde1cbd37817c0a9a381a9a3
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2022-09-21 19:01:34 +0900

    Add files via upload

--- js/reader.js
+++ js/reader.js
@@ -1,4 +1,4 @@
-﻿var value = document.getElementById("code");
+﻿var codevalue = document.getElementById("code");
 var barcode = document.getElementById('barcode');
 var qrcode = document.getElementById('qrcode');
 var reset = document.getElementById('reset');
@@ -25,7 +25,6 @@ barcode.addEventListener('click', () => {
     var video, videostream, tmp, tmp_ctx, prev, prev_ctx, w, h, mw, mh, x1, y1;
     var DetectedCount = 0, DetectedCode = "";
 
-    //mask.style.display = 'inline';
     scanarea.style.display = 'inline';
 
     video = document.createElement('video');
@@ -41,13 +40,13 @@ barcode.addEventListener('click', () => {
     tmp = document.createElement('canvas');
     tmp_ctx = tmp.getContext("2d");
 
-    value.value = "";
+    codevalue.value = "";
 
-    value.style.width = "300px";
-    value.style.height = "36px";
-    value.style.display = "none";
-    value.style.overflow = "hidden";
-    value.style.textAlign = "center";
+    codevalue.style.width = "300px";
+    codevalue.style.height = "36px";
+    codevalue.style.display = "none";
+    codevalue.style.overflow = "hidden";
+    codevalue.style.textAlign = "center";
 
     //マイクはオフ, カメラの設定   背面カメラを希望する 640×480を希望する
     var options = { audio: false, video: { facingMode: "environment", width: { ideal: VideoSize[0] }, height: { ideal: VideoSize[1] } }};
@@ -67,10 +66,10 @@ barcode.addEventListener('click', () => {
             scanarea.style.display = 'none';
             barcode.style.display = "none";
             qrcode.style.display = "none";
-            value.style.display = "inline";
+            codevalue.style.display = "inline";
             reset.style.display = "inline";
 
-            value.value = err;
+            codevalue.value = err;
         }
     );
 
@@ -80,16 +79,13 @@ barcode.addEventListener('click', () => {
         barcode.style.display = "inline";
         qrcode.style.display = "inline";
         reset.style.display = "none";
-        value.style.display = "none";
+        codevalue.style.display = "none";
         scanarea.style.display = 'none';
     };
 
     function Scan(first) {
 
         if (first) {
-
-            //mask.style.display = 'none';
-
             //選択された幅高さ
             //w = video.videoWidth;
             //h = video.videoHeight;
@@ -175,11 +171,11 @@ barcode.addEventListener('click', () => {
         }
 
         if (DetectedCount >= 3) {
-            value.value = result.codeResult.code;
+            codevalue.value = result.codeResult.code;
 
             displayreset();
 
-            value.style.display = "inline";
+            codevalue.style.display = "inline";
             reset.style.display = "inline";
             scanarea.style.display = 'none';
             barcode.style.display = "none";
@@ -213,28 +209,55 @@ barcode.addEventListener('click', () => {
 
 qrcode.addEventListener('click', () => {
 
+    var VideoSize = new Array(1080, 720);
+    var SizeRate = 0.5;
+
     barcode.style.display = "none";
     qrcode.style.display = "none";
 
-    scanarea = document.createElement('div');
-    scanarea.id = "scanarea";
-    scanarea.className = "scanarea";
-    document.body.appendChild(scanarea);
-
-    var preview = document.createElement('canvas');
-    preview.id = "preview";
-    scanarea.appendChild(preview);
-
-    var info = document.createElement('p');
-    info.className = "text";
-    info.innerHTML = "カメラにバーコードを写してください。";
-    info.style.fontWeight = "1200";
-    info.style.marginBottom = "50px";
-    scanarea.appendChild(info);
-
-    var video, tmp, tmp_ctx, value, prev, prev_ctx, w, h, m, x1, y1;
+    var video, tmp, tmp_ctx, prev, prev_ctx, w, h, m, x1, y1;
 
     scanarea.style.display = 'inline';
+
+    video = document.createElement('video');
+    video.id = "video";
+    video.setAttribute("autoplay", "");
+    video.setAttribute("muted", "");
+    video.setAttribute("playsinline", "");
+    video.onloadedmetadata = function (e) { video.play(); };
+
+    prev = document.getElementById("preview");
+    prev_ctx = prev.getContext("2d");
+
+    tmp = document.createElement('canvas');
+    tmp_ctx = tmp.getContext("2d");
+
+    codevalue.value = "";
+
+    codevalue.style.width = "360px";
+    codevalue.style.height = "108px";
+    codevalue.style.display = "none";
+    codevalue.style.textAlign = "left";
+    codevalue.style.overflow = "scroll";
+
+    //scanarea = document.createElement('div');
+    //scanarea.id = "scanarea";
+    //scanarea.className = "scanarea";
+    //document.body.appendChild(scanarea);
+
+    //var preview = document.createElement('canvas');
+    //preview.id = "preview";
+    //scanarea.appendChild(preview);
+
+    //var info = document.createElement('p');
+    //info.className = "text";
+    //info.innerHTML = "カメラにバーコードを写してください。";
+    //info.style.fontWeight = "1200";
+    //info.style.marginBottom = "50px";
+    //scanarea.appendChild(info);
+
+
+    //scanarea.style.display = 'inline';
 
     video = document.createElement('video');
     video.setAttribute("autoplay", "");
@@ -248,21 +271,26 @@ qrcode.addEventListener('click', () => {
     tmp = document.createElement('canvas');
     tmp_ctx = tmp.getContext("2d");
 
-    value = document.getElementById("code");
-    value.value = "";
+    //value = document.getElementById("code");
+    //value.value = "";
 
-    value.style.width = "360px";
-    value.style.height = "108px";
-    value.style.display = "none";
-    value.style.textAlign = "left";
-    value.style.overflow = "scroll";
+    //value.style.width = "360px";
+    //value.style.height = "108px";
+    //value.style.display = "none";
+    //value.style.textAlign = "left";
+    //value.style.overflow = "scroll";
+
+    //マイクはオフ, カメラの設定   背面カメラを希望する 640×480を希望する
+    var options = { audio: false, video: { facingMode: "environment", width: { ideal: VideoSize[0] }, height: { ideal: VideoSize[1] } } };
 
     //カメラ使用の許可ダイアログが表示される
     navigator.mediaDevices.getUserMedia(
         //マイクはオフ, カメラの設定   できれば背面カメラ    できれば640×480
-        { "audio": false, "video": { "facingMode": "environment", "width": { "ideal": 1080 }, "height": { "ideal": 720 } } }
+    //    { "audio": false, "video": { "facingMode": "environment", "width": { "ideal": 1080 }, "height": { "ideal": 720 } } }
+        options
     ).then( //許可された場合
         function (stream) {
+            videostream = stream;
             video.srcObject = stream;
             //0.5秒後にスキャンする
             id = setTimeout(Scan, 500, true);
@@ -271,29 +299,65 @@ qrcode.addEventListener('click', () => {
         function (err) {
             scanarea.style.display = 'none';
 
-            value.style.display = "inline";
+            codevalue.style.display = "inline";
             reset.style.display = "inline";
-            value.value = err;
+            codevalue.value = err;
         }
     );
 
+    turn.onclick = function () {
+
+        displayreset();
+        barcode.style.display = "inline";
+        qrcode.style.display = "inline";
+        reset.style.display = "none";
+        codevalue.style.display = "none";
+        scanarea.style.display = 'none';
+    };
+
     function Scan(first) {
 
-        if (first) {
-
-            var turn = document.createElement('button');
-            turn.className = "turn";
-            turn.innerHTML = "戻る";
-            scanarea.appendChild(turn);
-
-            turn.onclick = function () {
-                location.reload();
-            };
-        }
+        //if (first) {
+        //    if (window.innerWidth < window.innerHeight) {
+        //        if (video.videoWidth < video.videoHeight) {
+        //            w = video.videoWidth;
+        //            h = video.videoHeight;
+        //        } else {
+        //            h = video.videoWidth;
+        //            w = video.videoHeight;
+        //        }
+        //    } else {
+        //        if (video.videoWidth < video.videoHeight) {
+        //            h = video.videoWidth;
+        //            w = video.videoHeight;
+        //        } else {
+        //            w = video.videoWidth;
+        //            h = video.videoHeight;
+        //        }
+        //    }
+        //}
 
         //選択された幅高さ
-        w = video.videoWidth;
-        h = video.videoHeight;
+        //w = video.videoWidth;
+        //h = video.videoHeight;
+
+        if (window.innerWidth < window.innerHeight) {
+            if (video.videoWidth < video.videoHeight) {
+                w = video.videoWidth;
+                h = video.videoHeight;
+            } else {
+                h = video.videoWidth;
+                w = video.videoHeight;
+            }
+        } else {
+            if (video.videoWidth < video.videoHeight) {
+                h = video.videoWidth;
+                w = video.videoHeight;
+            } else {
+                w = video.videoWidth;
+                h = video.videoHeight;
+            }
+        }
 
         //画面上の表示サイズ
         prev.style.width = (w / 2) + "px";
@@ -324,14 +388,19 @@ qrcode.addEventListener('click', () => {
         let scanResult = jsQR(imageData.data, m, m);
 
         if (scanResult) {
-
-            clearTimeout(id);
-
             //QRコードをスキャンした結果を出力
-            value.value = scanResult.data;
-            value.scrollTop = value.scrollHeight;
+            codevalue.value = scanResult.data;
+            codevalue.scrollTop = codevalue.scrollHeight;
 
             displayreset();
+
+            codevalue.style.display = "inline";
+            reset.style.display = "inline";
+            scanarea.style.display = 'none';
+            barcode.style.display = "none";
+            qrcode.style.display = "none";
+
+            clearTimeout(id);
         }
 
         id = setTimeout(Scan, 50, false);
@@ -339,18 +408,34 @@ qrcode.addEventListener('click', () => {
 
     function displayreset() {
 
-        scanarea.style.display = 'none';
+        Quagga.stop();
+        clearTimeout(id);
 
-        value.style.display = "inline";
-        reset.style.display = "inline";
+        const tracks = videostream.getVideoTracks();
+        for (let i = 0; i < tracks.length; i++) {
+            tracks[i].stop();
+        }
 
+        prev_ctx.clearRect(0, 0, w, h);
+        tmp_ctx.clearRect(x1, y1, m, m, 0, 0, m, m);
+
+        DetectedCode = '';
+        DetectedCount = 0;
+        video.remove();
         tmp.remove();
 
-        video.stop();
-        video.srcObject = null;
+        //scanarea.style.display = 'none';
 
-        video.remove();
-        scanarea.remove();
+        //value.style.display = "inline";
+        //reset.style.display = "inline";
+
+        //tmp.remove();
+
+        //video.stop();
+        //video.srcObject = null;
+
+        //video.remove();
+        //scanarea.remove();
     }
 });
 
@@ -361,6 +446,6 @@ reset.addEventListener('click', () => {
     barcode.style.display = "inline";
     qrcode.style.display = "inline";
     reset.style.display = "none";
-    value.style.display = "none";
+    codevalue.style.display = "none";
     scanarea.style.display = 'none';
 });
