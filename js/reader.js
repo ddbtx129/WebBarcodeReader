32aab5441fb09883b0e035b958cbcee46936d57d
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2022-09-24 02:59:56 +0900

    Add files via upload

--- js/reader.js
+++ js/reader.js
@@ -63,7 +63,6 @@ barcode.addEventListener('click', () => {
 
     prev = document.getElementById("preview");
     prev_ctx = prev.getContext("2d");
-    prev_ctx2 = prev.getContext("2d");
 
     tmp = document.createElement('canvas');
     tmp_ctx = tmp.getContext("2d");
@@ -152,12 +151,9 @@ barcode.addEventListener('click', () => {
         prev_ctx.strokeStyle = "rgb(255,0,0)";
         prev_ctx.lineWidth = 3;
         prev_ctx.rect(((w - (w * ScanRate[0])) / 2), ((h - (w * ScanRate[1])) / 2), (w * ScanRate[0]), (w * ScanRate[1]));
+        prev_ctx.moveTo(((w - (w * ScanRate[0])) / 2) - 60, ((h - (w * ScanRate[1])) / 2) + ((w * ScanRate[1]) / 2));
+        prev_ctx.lineTo(((w - (w * ScanRate[0])) / 2) + 120, ((h - (w * ScanRate[1])) / 2) + ((w * ScanRate[1]) / 2));
         prev_ctx.stroke();
-        prev_ctx2.beginPath();
-        prev_ctx2.strokeStyle = "rgb(255,255,255)";
-        prev_ctx2.moveTo(((w - (w * ScanRate[0])) / 2) - 60, ((h - (w * ScanRate[1])) / 2) + ((h - (w * ScanRate[1])) / 4));
-        prev_ctx2.lineTo(((w - (w * ScanRate[0])) / 2) + 120, ((h - (w * ScanRate[1])) / 2) + ((h - (w * ScanRate[1])) / 4));
-        prev_ctx2.stroke();
 
         tmp.setAttribute("width", (w * ScanRate[0]));
         tmp.setAttribute("height", (w * ScanRate[1]));
