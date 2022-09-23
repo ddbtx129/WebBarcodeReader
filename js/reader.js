fafc915337dbee1ff9cfad164354531a90a23b4a
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2022-09-24 02:46:25 +0900

    Add files via upload

--- js/reader.js
+++ js/reader.js
@@ -151,9 +151,10 @@ barcode.addEventListener('click', () => {
         prev_ctx.strokeStyle = "rgb(255,0,0)";
         prev_ctx.lineWidth = 3;
         prev_ctx.rect(((w - (w * ScanRate[0])) / 2), ((h - (w * ScanRate[1])) / 2), (w * ScanRate[0]), (w * ScanRate[1]));
+        prev_ctx.stroke();
         prev_ctx.strokeStyle = "rgb(255,255,255)";
-        prev_ctx.moveTo(((w - (w * ScanRate[0])) / 2) - 80, (h - (w * ScanRate[1])));
-        prev_ctx.lineTo((w * ScanRate[0]) + 80, (h - (w * ScanRate[1])));
+        prev_ctx.moveTo(((w - (w * ScanRate[0])) / 2) - 50, ((h - (w * ScanRate[1])) / 2));
+        prev_ctx.lineTo((w * ScanRate[0]) + 100, ((h - (w * ScanRate[1])) / 2));
         prev_ctx.stroke();
 
         tmp.setAttribute("width", (w * ScanRate[0]));
