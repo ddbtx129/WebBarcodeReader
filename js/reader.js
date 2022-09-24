f0dd983fa3d499a08544db07c27c207977804b09
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2022-09-24 21:20:19 +0900

    Add files via upload

--- js/reader.js
+++ js/reader.js
@@ -149,9 +149,9 @@ barcode.addEventListener('click', () => {
         prev_ctx.drawImage(video, 0, 0, w, h);
 
         prev_ctx.beginPath();
-        prev_ctx.strokeStyle = "rgb(255,255,255,0.5)";
+        prev_ctx.strokeStyle = "rgb(255,255,255,0.4)";
         prev_ctx.lineWidth = 2;
-        prev_ctx.setLineDash([10, 20]);
+        //prev_ctx.setLineDash([10, 20]);
         prev_ctx.moveTo(((w - (w * ScanRate[0])) / 2) - 50, ((h - (w * ScanRate[1])) / 2) + ((w * ScanRate[1]) / 2));
         prev_ctx.lineTo(((w - (w * ScanRate[0])) / 2) + (w * ScanRate[0]) + 50, ((h - (w * ScanRate[1])) / 2) + ((w * ScanRate[1]) / 2));
 
@@ -364,9 +364,9 @@ qrcode.addEventListener('click', () => {
         prev_ctx.drawImage(video, 0, 0, w, h);
 
         prev_ctx.beginPath();
-        prev_ctx.strokeStyle = "rgb(255,255,255,0.5)";
+        prev_ctx.strokeStyle = "rgb(255,255,255,0.4)";
         prev_ctx.lineWidth = 2;
-        prev_ctx.setLineDash([10, 10]);
+        //prev_ctx.setLineDash([10, 10]);
         prev_ctx.moveTo(x1 - 50, y1 + (m / 2));
         prev_ctx.lineTo((x1 + m + 50), y1 + (m / 2));
 
@@ -374,9 +374,9 @@ qrcode.addEventListener('click', () => {
         prev_ctx.stroke();
 
         prev_ctx.beginPath();
-        prev_ctx.strokeStyle = "rgb(255,255,255,0.5)";
+        prev_ctx.strokeStyle = "rgb(255,255,255,0.4)";
         prev_ctx.lineWidth = 2;
-        prev_ctx.setLineDash([10, 10]);
+        //prev_ctx.setLineDash([10, 10]);
         prev_ctx.moveTo(x1 + (m / 2), y1 - 50);
         prev_ctx.lineTo(x1 + (m / 2), y1 + m + 50);
 
