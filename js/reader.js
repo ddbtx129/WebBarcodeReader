c62fc9a2230087cb99bc6a33c3ae7b80688145f2
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2022-09-24 21:21:55 +0900

    Add files via upload

--- js/reader.js
+++ js/reader.js
@@ -161,7 +161,7 @@ barcode.addEventListener('click', () => {
         prev_ctx.beginPath();
         prev_ctx.strokeStyle = "rgb(255,0,0)";
         prev_ctx.lineWidth = 2;
-        prev_ctx.setLineDash([]);
+        prev_ctx.setLineDash([16, 16]);
         prev_ctx.rect(((w - (w * ScanRate[0])) / 2), ((h - (w * ScanRate[1])) / 2), (w * ScanRate[0]), (w * ScanRate[1]));
 
         prev_ctx.closePath();
@@ -386,7 +386,7 @@ qrcode.addEventListener('click', () => {
         prev_ctx.beginPath();
         prev_ctx.strokeStyle = "rgb(255,0,0)";
         prev_ctx.lineWidth = 2;
-        prev_ctx.setLineDash([]);
+        prev_ctx.setLineDash([5, 5]);
         prev_ctx.rect(x1, y1, m, m);
 
         prev_ctx.closePath();
