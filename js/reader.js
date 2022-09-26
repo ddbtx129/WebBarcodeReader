9d1e3c01be2963f2ea0dc38c3ff12ad46a412a7b
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2022-09-26 09:27:04 +0900

    Add files via upload

--- js/reader.js
+++ js/reader.js
@@ -47,6 +47,7 @@ barcode.addEventListener('click', () => {
     var tranc = 1;
     var trancFlg = 0.0;
     var searchline = 0;
+    var searchline2 = 0;
     var searchlinemove = 25;
 
     barcode.style.display = "none";
@@ -152,6 +153,7 @@ barcode.addEventListener('click', () => {
 
         tranc = tranc + trancFlg;
         searchline = searchline + searchlinemove;
+        searchline2 = searchline2 + searchlinemove;
 
         prev_ctx.drawImage(video, 0, 0, w, h);
 
@@ -170,13 +172,13 @@ barcode.addEventListener('click', () => {
         // 
         prev_ctx.beginPath();
         // 線形グラデーション
-        var g = prev_ctx.createLinearGradient(((w - (w * ScanRate[0])) / 2) - 50 + searchline , ((h - (w * ScanRate[1])) / 2), searchline, (w * ScanRate[1]));
+        var g = prev_ctx.createLinearGradient(((w - (w * ScanRate[0])) / 2) - 50 + searchline2, ((h - (w * ScanRate[1])) / 2), searchline, (w * ScanRate[1]));
         // 色を定義
         g.addColorStop(0, 'rgb(255,255,255,0)');
         g.addColorStop(0.4, 'rgb(255,255,255,0.25)');
         g.addColorStop(1, 'rgb(255,255,255,0.5)');
         prev_ctx.fillStyle = g;
-        prev_ctx.fillRect(((w - (w * ScanRate[0])) / 2) - 50 + searchline, ((h - (w * ScanRate[1])) / 2), searchline, (w * ScanRate[1]));
+        prev_ctx.fillRect(((w - (w * ScanRate[0])) / 2) - 50 + searchline2, ((h - (w * ScanRate[1])) / 2), searchline, (w * ScanRate[1]));
         //prev_ctx.strokeStyle = "rgb(255,255,255," + tranc + ")";
         //prev_ctx.lineWidth = 2;
         //prev_ctx.setLineDash([2, 2]);
@@ -236,6 +238,7 @@ barcode.addEventListener('click', () => {
 
         if (searchline >= ((h - (w * ScanRate[1])) / 2) + ((w * ScanRate[1]) / 2)) {
             searchline = ((w - (w * ScanRate[0])) / 2);
+            searchline2 = 0;
         }
 
         id = setTimeout(Scan, 50, flg);
