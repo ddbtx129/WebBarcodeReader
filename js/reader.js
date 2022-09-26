a643c4bbf1126f71e01ea4d1cb3de8ce8770af96
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2022-09-26 09:15:15 +0900

    Add files via upload

--- js/reader.js
+++ js/reader.js
@@ -170,13 +170,13 @@ barcode.addEventListener('click', () => {
         // 
         prev_ctx.beginPath();
         // 線形グラデーション
-        var g = prev_ctx.createLinearGradient(((w - (w * ScanRate[0])) / 2) - 50, ((h - (w * ScanRate[1])) / 2), searchline, (w * ScanRate[1]));
+        var g = prev_ctx.createLinearGradient(((w - (w * ScanRate[0])) / 2) - 50 + searchline , ((h - (w * ScanRate[1])) / 2), searchline, (w * ScanRate[1]));
         // 色を定義
         g.addColorStop(0, 'rgb(255,255,255,0)');
-        g.addColorStop(0.4, 'rgb(255,255,255,0.5)');
-        g.addColorStop(1, 'rgb(255,255,255,0.75)');
+        g.addColorStop(0.4, 'rgb(255,255,255,0.25)');
+        g.addColorStop(1, 'rgb(255,255,255,0.5)');
         prev_ctx.fillStyle = g;
-        prev_ctx.fillRect(((w - (w * ScanRate[0])) / 2) - 50, ((h - (w * ScanRate[1])) / 2), searchline, (w * ScanRate[1]));
+        prev_ctx.fillRect(((w - (w * ScanRate[0])) / 2) - 50 + searchline, ((h - (w * ScanRate[1])) / 2), searchline, (w * ScanRate[1]));
         //prev_ctx.strokeStyle = "rgb(255,255,255," + tranc + ")";
         //prev_ctx.lineWidth = 2;
         //prev_ctx.setLineDash([2, 2]);
