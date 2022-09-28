4f1de89463bb1d267fbb4d07f0ba7597cf789c61
ddbtx129 <63324152+ddbtx129@users.noreply.github.com>
2022-09-28 17:15:59 +0900

    Add files via upload

--- js/barcode.js
+++ js/barcode.js
@@ -266,7 +266,7 @@ barcode.addEventListener('click', () => {
                 searchline += searchlinemove;
             }
 
-            console.log(String(searchline) + " / " + String((w * ScanRate[0])) + "    " + String(((w - (w * ScanRate[0])) / 2) + searchline) + " : " + string(searchNum));
+            console.log(String(searchline) + " / " + String((w * ScanRate[0])) + "    " + String(((w - (w * ScanRate[0])) / 2) + searchline) + " : " + String(searchNum));
         }
 
         id = setTimeout(Scan, loopspan, flg);
